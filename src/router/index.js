d109fb9e03d4ddb0ed36ed7b7013dd448dffc56e
deni-b-gomes <carol.junko@gmail.com>
2026-07-09 17:42:16 +0200

    "feat(router): transition to strict parameter routing to support language-level trajectory context"

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LanguagePickerView from '@/views/LanguagePickerView.vue'
-import CategoryView from '@/views/CategoryView.vue'
-import ActivityHubView from '@/views/ActivityHubView.vue'
-import ExerciseView from '@/views/ExerciseView.vue'
-import DeckView from '@/views/DeckView.vue'
+import LanguagePickerView from '../views/LanguagePickerView.vue'
+import CategoryView from '../views/CategoryView.vue'
+import ExerciseView from '../views/ExerciseView.vue'
+import FlashcardsView from '../views/FlashcardsView.vue'
+// import GrammarPractice from '../views/GrammarPractice.vue'
 
-
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView,
+  },
+  {
+    path: '/languages',
+    name: 'LanguagePicker',
+    component: LanguagePickerView,
+  },
+  {
+    path: '/category/:langId',
+    name: 'Category',
+    component: CategoryView,
+  },
+  {
+    path: '/hub/:deckId',
+    name: 'ExerciseHub',
+    component: ExerciseView,
+  },
+  {
+    path: '/hub/:deckId/flashcards',
+    name: 'Flashcards',
+    component: FlashcardsView,
+  },
+  // {
+  //   path: '/hub/:deckId/grammar',
+  //   name: 'GrammarPractice',
+  //   component: GrammarPractice,
+  // },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/deck/:deckID',
-      name: 'deck',
-      component: DeckView,
-    },
-    {
-      path: '/languages',
-      name: 'languages',
-      component: LanguagePickerView,
-    },
-    {
-      path: '/setup/:langId',
-      name: 'setup',
-      component: CategoryView,
-    },
-    {
-      path: '/hub/:deckId',
-      name: 'hub',
-      component: ActivityHubView,
-    },
-    {
-      path: '/exercises/:deckId',
-      name: 'exercises',
-      component: ExerciseView,
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
